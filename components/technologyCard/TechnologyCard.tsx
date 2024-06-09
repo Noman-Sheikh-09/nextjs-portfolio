6fe31625a4f0c1acae455a92f91f75c6dbc18b29
Noman Sheikh <94075289+Noman-Sheikh-09@users.noreply.github.com>
2024-06-09 14:29:59 +0500

    fix type issue

--- components/technologyCard/TechnologyCard.tsx
+++ components/technologyCard/TechnologyCard.tsx
@@ -4,6 +4,7 @@ import React from "react";
 interface Item {
   title: string;
   percentage: string;
+  logo: any;
 }
 
 // Step 2: Update the component to use the defined interface
@@ -14,9 +15,7 @@ interface TechCardProps {
 const TechnologyCard: React.FC<TechCardProps> = ({ item }) => {
   return (
     <div className="w-[100%] h-[200px] flex flex-col items-center justify-center rounded-[10px]  border-[0.5px] cursor-pointer shadow-md bg-white px-[5%]">
-     <div>
-      {item?.logo}
-     </div>
+      <div>{item?.logo}</div>
       <p className="font-bold text-[1.2rem] text-black">{item.title}</p>
       {/* <p>{item.percentage}</p> */}
     </div>
